59d5c442fa4305b7c76c3880629476ce733a31e2
dan7sc <su7cheng@gmail.com>
2019-05-23 17:45:47 -0300

    Refactor: switch to using AsyncTaskLoader instead of an AsyncTask.

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeLoader.kt
@@ -0,0 +1,44 @@
+@file:Suppress("DEPRECATION")
+
+package com.example.android.quakereport
+
+
+import android.content.AsyncTaskLoader
+import android.content.Context
+
+/**
+ * Loads a list of earthquakes by using an AsyncTask to perform the
+ * network request to the given URL.
+ */
+class EarthquakeLoader
+/**
+ * Constructs a new [EarthquakeLoader].
+ *
+ * @param context of the activity
+ * @param url to load data from
+ */
+(context: Context, url: String) : AsyncTaskLoader<List<Earthquake>>(context) {
+     /** Query URL  */
+     private val mUrl: String? = url
+
+    override fun onStartLoading() {
+        forceLoad()
+    }
+
+    /**
+     * This is on a background thread.
+     */
+    override fun loadInBackground(): List<Earthquake>? {
+        return if (mUrl == null) {
+            null
+        } else QueryUtils.fetchEarthquakeData(mUrl)
+
+        // Perform the network request, parse the response, and extract a list of earthquakes.
+    }
+
+    companion object {
+
+        /** Tag for log messages  */
+        private val LOG_TAG = EarthquakeLoader::class.java.name
+    }
+}

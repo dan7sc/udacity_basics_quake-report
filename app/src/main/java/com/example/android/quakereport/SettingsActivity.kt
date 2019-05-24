182292a2f6b4c2ad04ef9b578b39cb3cf43a3a7d
dan7sc <su7cheng@gmail.com>
2019-05-24 12:42:15 -0300

    Display preference summaries.

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.kt
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.kt
@@ -5,6 +5,10 @@ package com.example.android.quakereport
 import android.os.Bundle
 import android.preference.PreferenceFragment
 import android.support.v7.app.AppCompatActivity
+import android.preference.PreferenceManager
+import android.content.SharedPreferences
+import android.preference.Preference
+
 
 
 class SettingsActivity : AppCompatActivity() {
@@ -14,10 +18,28 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.settings_activity)
     }
 
-    class EarthquakePreferenceFragment : PreferenceFragment() {
+    class EarthquakePreferenceFragment : PreferenceFragment(), Preference.OnPreferenceChangeListener {
+
         override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        addPreferencesFromResource(R.xml.settings_main)
+            super.onCreate(savedInstanceState)
+            addPreferencesFromResource(R.xml.settings_main)
+
+            val minMagnitude: Preference = findPreference(getString(R.string.settings_min_magnitude_key))
+            bindPreferenceSummaryToValue(minMagnitude)
+        }
+
+        override fun onPreferenceChange(preference: Preference, value: Any?): Boolean {
+            val stringValue: String = value!!.toString()
+            preference.summary = stringValue
+            return true
+        }
+
+        private fun bindPreferenceSummaryToValue(preference: Preference) {
+            preference.onPreferenceChangeListener = this
+            val preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(preference.context)
+            val preferenceString: String? = preferences.getString(preference.key, "")
+            onPreferenceChange(preference, preferenceString)
         }
     }
+
 }
